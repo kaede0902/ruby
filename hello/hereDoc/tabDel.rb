61631d24fd1b9248830ed122e68f20a385f1d108
kaede0902 <kaede0920js@gmail.com>
2019-07-21 16:02:36 +0900

    add EOS del

--- hello/hereDoc/tabDel.rb
+++ hello/hereDoc/tabDel.rb
@@ -1,7 +1,8 @@
 print <<-EOS
   indented string
-          next tabbed string
-          no tab string
-EOS
+    next tabbed string
+  no tab string
+  EOS
 # with `<<~` clean tab?
 # <<- will not delete tab
+# But delete tab before EOS.
